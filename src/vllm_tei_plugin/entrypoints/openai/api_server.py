f3a5b120ddc74681838ab6cc7bff0c4ecf61f648
Monad Kai <onlookerliu_sjtu@163.com>
2025-08-11 19:46:37 -0700

    :art: Clean code

--- src/vllm_tei_plugin/entrypoints/openai/api_server.py
+++ src/vllm_tei_plugin/entrypoints/openai/api_server.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 # SPDX-FileCopyrightText: Copyright contributors to the vLLM project
+import signal
 from argparse import Namespace
 from http import HTTPStatus
 from typing import Optional
@@ -10,51 +11,30 @@ from fastapi.responses import JSONResponse
 from starlette.datastructures import State
 from vllm.config import VllmConfig
 from vllm.engine.protocol import EngineClient
-from vllm.entrypoints.chat_utils import (
-    load_chat_template,
-    resolve_hf_chat_template,
-    resolve_mistral_chat_template,
-)
+from vllm.entrypoints.chat_utils import load_chat_template
 from vllm.entrypoints.launcher import serve_http
 from vllm.entrypoints.logger import RequestLogger
 from vllm.entrypoints.openai.api_server import (
+    TIMEOUT_KEEP_ALIVE,
     ErrorResponse,
     base,
     build_app,
     build_async_engine_client,
+    create_server_socket,
+    init_app_state,
     logger,
     validate_json_request,
 )
 from vllm.entrypoints.openai.cli_args import make_arg_parser, validate_parsed_serve_args
-from vllm.reasoning import ReasoningParserManager
-from vllm.version import __version__ as VLLM_VERSION
-
-# yapf: enable
-from vllm.entrypoints.openai.serving_chat import OpenAIServingChat
-from vllm.entrypoints.openai.serving_completion import OpenAIServingCompletion
-from vllm.entrypoints.openai.serving_embedding import OpenAIServingEmbedding
-from vllm.entrypoints.openai.serving_models import (
-    BaseModelPath,
-    OpenAIServingModels,
-)
-from vllm.entrypoints.openai.serving_pooling import OpenAIServingPooling
-from vllm.entrypoints.openai.serving_score import ServingScores
-from vllm.entrypoints.openai.serving_tokenization import OpenAIServingTokenization
-from vllm.entrypoints.openai.serving_transcription import OpenAIServingTranscription
-import signal
-from vllm.entrypoints.openai.api_server import create_server_socket, TIMEOUT_KEEP_ALIVE
-from vllm.utils import (FlexibleArgumentParser,
-                        is_valid_ipv6_address, set_ulimit)
 from vllm.entrypoints.openai.tool_parsers import ToolParserManager
 from vllm.entrypoints.utils import cli_env_setup, load_aware_call, with_cancellation
-from vllm.transformers_utils.tokenizer import MistralTokenizer
+from vllm.reasoning import ReasoningParserManager
+from vllm.utils import FlexibleArgumentParser, is_valid_ipv6_address, set_ulimit
+from vllm.version import __version__ as VLLM_VERSION
 
 from vllm_tei_plugin.entrypoints.openai.serving_tei_embed import TeiServingEmbed
 from vllm_tei_plugin.entrypoints.openai.serving_tei_rerank import TeiServingRerank
-
-# yapf conflicts with isort for this block
-# yapf: disable
-from vllm_tei_plugin.protocol import (EmbedRequest, RerankRequest)
+from vllm_tei_plugin.protocol import EmbedRequest, RerankRequest
 
 tei_router = APIRouter(prefix="/tei")
 
@@ -79,125 +59,16 @@ async def extended_init_app_state(
     state: State,
     args: Namespace,
 ) -> None:
-    if args.served_model_name is not None:
-        served_model_names = args.served_model_name
-    else:
-        served_model_names = [args.model]
+    await init_app_state(engine_client, vllm_config, state, args)
 
     if args.disable_log_requests:
         request_logger = None
     else:
         request_logger = RequestLogger(max_log_len=args.max_log_len)
 
-    base_model_paths = [
-        BaseModelPath(name=name, model_path=args.model)
-        for name in served_model_names
-    ]
-
-    state.engine_client = engine_client
-    state.log_stats = not args.disable_log_stats
-    state.vllm_config = vllm_config
     model_config = vllm_config.model_config
 
     resolved_chat_template = load_chat_template(args.chat_template)
-    if resolved_chat_template is not None:
-        # Get the tokenizer to check official template
-        tokenizer = await engine_client.get_tokenizer()
-
-        if isinstance(tokenizer, MistralTokenizer):
-            # The warning is logged in resolve_mistral_chat_template.
-            resolved_chat_template = resolve_mistral_chat_template(
-                chat_template=resolved_chat_template)
-        else:
-            hf_chat_template = resolve_hf_chat_template(
-                tokenizer,
-                chat_template=None,
-                tools=None,
-                trust_remote_code=model_config.trust_remote_code)
-
-            if hf_chat_template != resolved_chat_template:
-                logger.warning(
-                    "Using supplied chat template: %s\n"
-                    "It is different from official chat template '%s'. "
-                    "This discrepancy may lead to performance degradation.",
-                    resolved_chat_template, args.model)
-
-    state.openai_serving_models = OpenAIServingModels(
-        engine_client=engine_client,
-        model_config=model_config,
-        base_model_paths=base_model_paths,
-        lora_modules=args.lora_modules,
-        prompt_adapters=args.prompt_adapters,
-    )
-    await state.openai_serving_models.init_static_loras()
-    state.openai_serving_chat = OpenAIServingChat(
-        engine_client,
-        model_config,
-        state.openai_serving_models,
-        args.response_role,
-        request_logger=request_logger,
-        chat_template=resolved_chat_template,
-        chat_template_content_format=args.chat_template_content_format,
-        return_tokens_as_token_ids=args.return_tokens_as_token_ids,
-        enable_auto_tools=args.enable_auto_tool_choice,
-        tool_parser=args.tool_call_parser,
-        enable_reasoning=args.enable_reasoning,
-        reasoning_parser=args.reasoning_parser,
-        enable_prompt_tokens_details=args.enable_prompt_tokens_details,
-    ) if model_config.runner_type == "generate" else None
-    state.openai_serving_completion = OpenAIServingCompletion(
-        engine_client,
-        model_config,
-        state.openai_serving_models,
-        request_logger=request_logger,
-        return_tokens_as_token_ids=args.return_tokens_as_token_ids,
-    ) if model_config.runner_type == "generate" else None
-    state.openai_serving_pooling = OpenAIServingPooling(
-        engine_client,
-        model_config,
-        state.openai_serving_models,
-        request_logger=request_logger,
-        chat_template=resolved_chat_template,
-        chat_template_content_format=args.chat_template_content_format,
-    ) if model_config.runner_type == "pooling" else None
-    state.openai_serving_embedding = OpenAIServingEmbedding(
-        engine_client,
-        model_config,
-        state.openai_serving_models,
-        request_logger=request_logger,
-        chat_template=resolved_chat_template,
-        chat_template_content_format=args.chat_template_content_format,
-    ) if model_config.task == "embed" else None
-    state.openai_serving_scores = ServingScores(
-        engine_client,
-        model_config,
-        state.openai_serving_models,
-        request_logger=request_logger) if model_config.task in (
-            "score", "embed", "pooling") else None
-    state.jinaai_serving_reranking = ServingScores(
-        engine_client,
-        model_config,
-        state.openai_serving_models,
-        request_logger=request_logger
-    ) if model_config.task == "score" else None
-    state.openai_serving_tokenization = OpenAIServingTokenization(
-        engine_client,
-        model_config,
-        state.openai_serving_models,
-        request_logger=request_logger,
-        chat_template=resolved_chat_template,
-        chat_template_content_format=args.chat_template_content_format,
-    )
-    state.openai_serving_transcription = OpenAIServingTranscription(
-        engine_client,
-        model_config,
-        state.openai_serving_models,
-        request_logger=request_logger,
-    ) if model_config.runner_type == "transcription" else None
-    state.task = model_config.task
-
-    state.enable_server_load_tracking = args.enable_server_load_tracking
-    state.server_load_metrics = 0
 
     # Here is the difference
     state.tei_serving_embed = (
